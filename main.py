f6af8c133f762acfef0aa521854f404d83673902
Jerome Swannack <jeromeswannack@gmail.com>
2022-12-28 16:21:15 +1300

    Scaffolded project

---
+++ main.py
@@ -0,0 +1,9 @@
+def main():
+    image_path = "/home/jerome/phone_backup/DCIM/Camera/20170804_203506.jpg"
+
+
+if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(level=logging.INFO)
+    main()
